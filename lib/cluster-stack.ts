b1271864454a665477232cacbf3238b6e82c02bc
Jakub Gruber <jgr@paymorrow.com>
2021-02-11 10:41:19 +0100

    Completed multi-region EKS by CDK workshop

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -2,24 +2,73 @@ import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import * as eks from '@aws-cdk/aws-eks';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import { PhysicalName } from '@aws-cdk/core';
+import {PhysicalName} from '@aws-cdk/core';
 
 export class ClusterStack extends cdk.Stack {
 
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    public readonly cluster: eks.Cluster;
+    public readonly firstRegionRole: iam.Role;
+    public readonly secondRegionRole: iam.Role;
 
-    const primaryRegion = 'ap-northeast-2';
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-  }
+        const primaryRegion = 'ap-northeast-2';
+
+        const clusterAdmin = new iam.Role(this, 'AdminRole', {
+            assumedBy: new iam.AccountRootPrincipal()
+        });
+
+        // TODO:
+        // new eks.FargateCluster(this, 'HelloEKS', {
+        //     version: eks.KubernetesVersion.V1_18,
+        //     clusterName: `demogo`,
+        //     mastersRole: clusterAdmin,
+        //     //defaultProfile: {}
+        // });
+
+        const cluster = new eks.Cluster(this, 'demogo-cluster', {
+            clusterName: `demogo`,
+            mastersRole: clusterAdmin,
+            version: eks.KubernetesVersion.V1_18,
+            defaultCapacity: 2,
+            defaultCapacityInstance: cdk.Stack.of(this).region == primaryRegion ?
+                new ec2.InstanceType('r5.xlarge') : new ec2.InstanceType('m5.2xlarge')
+        });
+
+        cluster.addAutoScalingGroupCapacity('spot-group', {
+            instanceType: new ec2.InstanceType('m5.xlarge'),
+            spotPrice: cdk.Stack.of(this).region == primaryRegion ? '0.248' : '0.192'
+        });
+
+        if (cdk.Stack.of(this).region == primaryRegion) {
+            this.firstRegionRole = createDeployRole(this, `for-1st-region`, cluster);
+        } else {
+            this.secondRegionRole = createDeployRole(this, `for-2nd-region`, cluster);
+        }
+
+        this.cluster = cluster;
+    }
 }
 
 function createDeployRole(scope: cdk.Construct, id: string, cluster: eks.Cluster): iam.Role {
-  const role = new iam.Role(scope, id, {
-    roleName: PhysicalName.GENERATE_IF_NEEDED,
-    assumedBy: new iam.AccountRootPrincipal()
-  });
-  cluster.awsAuth.addMastersRole(role);
+    const role = new iam.Role(scope, id, {
+        roleName: PhysicalName.GENERATE_IF_NEEDED,
+        assumedBy: new iam.AccountRootPrincipal()
+    });
+    cluster.awsAuth.addMastersRole(role);
 
-  return role;
+    return role;
+}
+
+
+export interface EksProps extends cdk.StackProps {
+    cluster: eks.Cluster
+}
+
+export interface CicdProps extends cdk.StackProps {
+    firstRegionCluster: eks.Cluster,
+    secondRegionCluster: eks.Cluster,
+    firstRegionRole: iam.Role,
+    secondRegionRole: iam.Role
 }
